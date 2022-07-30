c3951a538615d5138678cae197e2b824978f09a8
Poovarasan <mobtech.github@gmail.com>
2022-07-30 10:55:33 +0530

    Fixed unable to click start button on workout list page

--- app/src/main/java/com/mobtech/fitx360/activity/WorkoutListActivity.kt
+++ app/src/main/java/com/mobtech/fitx360/activity/WorkoutListActivity.kt
@@ -25,16 +25,7 @@ class WorkoutListActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_workout_list)
-
-        if (Build.VERSION.SDK_INT >= 23) {
-            Log.e("TAG", "onCreate::::223 " + Build.VERSION.SDK_INT)
-            val w: Window = window
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-        }
-
         mContext = this
 
         pWorkOutCategory =
